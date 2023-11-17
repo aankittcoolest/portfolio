dc3421441b099d346243e49efcadee915a26a4e8
Ankit Agrawal <aankittcoolest@gmail.com>
2023-11-17 12:31:26 +0800

    Login was breaking build. Fixing

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,16 @@
 import DefaultTheme from "vitepress/theme";
-import Layout from "./components/Layout.vue"
-import LoginButton from "./components/LoginButton.vue"
-import LogoutButton from "./components/LogoutButton.vue"
+// import Layout from "./components/Layout.vue"
+// import LoginButton from "./components/LoginButton.vue"
+// import LogoutButton from "./components/LogoutButton.vue"
 import "./custom.css";
 
 const Theme = {
     ...DefaultTheme,
-    Layout,
-    enhanceApp({app}: {app:any}) {
-        app.component("LoginButton", LoginButton),
-        app.component("LogoutButton", LoginButton)
-    }
+    // Layout,
+    // enhanceApp({app}: {app:any}) {
+    //     app.component("LoginButton", LoginButton),
+    //     app.component("LogoutButton", LogoutButton)
+    // }
 }
 
 export default Theme;
